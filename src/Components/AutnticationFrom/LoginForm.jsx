f67fb5bbfbec909496e0a2d983fa824ad5fc6e4c
ztalgorepublic <zeeshan.tanweer@algorepublic.com>
2024-09-12 18:26:34 +0500

    add login credentials

--- src/Components/AutnticationFrom/LoginForm.jsx
+++ src/Components/AutnticationFrom/LoginForm.jsx
@@ -3,8 +3,8 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 const LoginForm = () => {
-  const [username, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUserName] = useState("emilys");
+  const [password, setPassword] = useState("emilyspass");
   const navigate = useNavigate();
 
   const handleEmail = (event) => {
